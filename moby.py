ab2d81fc334f830a2fa16bd53a1619711d2fac85
Ashmin Bhandari <abhandar@ramapo.edu>
2023-01-12 02:40:57 -0500

    generate img based on ramdom sentence & post to instagram

--- moby.py
+++ moby.py
@@ -1,9 +1,15 @@
+from string import ascii_letters
+import textwrap
 import nltk.data
 import random
-import requests
 from PIL import Image, ImageDraw, ImageFont
+from instagrapi import Client
+
+FONT_URL = 'Bitter-ExtraBold.ttf'
 
 API_KEY = "b1313aaf-4bb1-4c55-98dc-e0c326cf2b63"
+
+IMG_NAME = "img.jpg"
 
 def getRandomSentence(txt_file):
 
@@ -18,24 +24,41 @@ def getRandomSentence(txt_file):
     newTok = random.choice([x for x in tok if len(x)<280])
 
     return newTok
+    
+def generageImage(txt, font_url):
+    img = Image.open(fp='background.jpg', mode='r')
+    
+    font = ImageFont.truetype(font=font_url, size=55)
+    
+    draw = ImageDraw.Draw(im=img)
 
-def generateImage(txt, file_name):
-    # Create a new image with a white background
-    image = Image.new('RGB', (200, 100), (255, 255, 255))
+    avg_char_width = sum(font.getsize(char)[0] for char in ascii_letters) / len(ascii_letters)
 
-    # Create a draw object to draw on the image
-    draw = ImageDraw.Draw(image)
- 
-    # Load the default font, and specify the font size
-    font = ImageFont.load_default()
+    max_char_count = int(img.size[0] * .900 / avg_char_width)
+    
+    text = textwrap.fill(text=txt, width=max_char_count)
 
-    # Draw the text on the image
-    draw.text((10, 10), txt, font=font, fill=(0, 0, 0))
+    bbox = draw.textbbox((img.size[0]/5, img.size[1]/3.5), text, font=font)
+    draw.rectangle(bbox, fill="#F0E0C0")
+    draw.text((img.size[0]/5, img.size[1]/3.5), text, font=font, fill="#3B3A28")
+    img.show()
+    img.save(IMG_NAME)
 
-    # Save the image to a file
-    image.save(file_name)
+def uploadImage(imgpth, caption):
+    cl = Client()
+    cl.login(USERNAME, PASSWORD)
+    cl.photo_upload(
+    imgpth,
+    caption,
+    extra_data={
+        "custom_accessibility_caption": caption,
+    }
+)
 
-prompt = getRandomSentence('moby-dick.txt')
-generateImage(prompt, 'image.png')
+
+txt = getRandomSentence('moby-dick.txt')
+generageImage(txt, FONT_URL)
+uploadImage(IMG_NAME, txt)
+
 
 
